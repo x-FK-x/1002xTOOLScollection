87a5a96b0119864f5d8cdea93f9cb1be23f9e77f
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2026-09-01 22:02:06 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-sed -i 's/\r//' "$0"
-if ! command -v sudo &> /dev/null; then
-    log "sudo not installed. Installing..."
-    apt update && apt install -y sudo | tee -a "$LOG_FILE"
-    if ! command -v sudo &> /dev/null; then
-        log "Failed to install sudo. Exiting."
-        exit 1
-    fi
+
+if ! command -v sudo &> /dev/null || ! sudo -v &> /dev/null; then
+  echo "Error: 'sudo' is not installed or the user lacks admin privileges. Exit."
+  exit 1
 fi
 
+echo "Success: Admin privileges verified!"
 
-TARGET_TOOLS_DIR="/etc/DODOS/tools"
+TARGET_TOOLS_DIR="/etc/dodos/tools"
 mkdir -p "$TARGET_TOOLS_DIR"
-mkdir -p /etc/DODOS/source
-echo "DEBIAN13" > /etc/DODOS/tools/osversion.txt
+mkdir -p /etc/dodos/source
+echo "DEBIAN13" > /etc/dodos/tools/osversion.txt
 
 
 LOG_FILE="$TARGET_TOOLS_DIR/1002xTOOLS_updater.log"
@@ -32,11 +29,11 @@ if ! command -v unzip &> /dev/null; then
     fi
 fi
 
-if ! command -v sudo &> /dev/null; then
-    log "sudo not installed. Installing..."
-    apt update && apt install -y sudo | tee -a "$LOG_FILE"
-    if ! command -v sudo &> /dev/null; then
-        log "Failed to install sudo. Exiting."
+if ! command -v curl &> /dev/null; then
+    log "curl not installed. Installing..."
+    sudo apt update && sudo apt install -y curl | tee -a "$LOG_FILE"
+    if ! command -v curl &> /dev/null; then
+        log "Failed to install unzip. Exiting."
         exit 1
     fi
 fi
@@ -98,7 +95,7 @@ else
 fi
 #-----
 
-LOCAL_CMD_FILE="/etc/DODOS/tools/1002xCMD-ver.txt"
+LOCAL_CMD_FILE="/etc/dodos/tools/1002xCMD-ver.txt"
 REMOTE_URL="https://raw.githubusercontent.com/x-FK-x/1002xCMD/refs/heads/main/version.txt"
 
 if [ -d "/etc/1002xCMD" ]; then
@@ -161,9 +158,9 @@ if ! command -v bc &> /dev/null; then
 fi
 
 # === Version erkennen ===
-if [[ -d /etc/DODOS ]]; then
+if [[ -d /etc/dodos ]]; then
     VERSION="dodos"
-    SCRIPT_DIR="/etc/DODOS"
+    SCRIPT_DIR="/etc/dodos"
 elif [[ -d /etc/modos ]]; then
     VERSION="modos"
     SCRIPT_DIR="/etc/modos"
@@ -174,7 +171,7 @@ else
 fi
 
 log "Detected version: $VERSION, SCRIPT_DIR: $SCRIPT_DIR"
-OS_VERSION=$(head -n1 "/etc/DODOS/tools/osversion.txt")
+OS_VERSION=$(head -n1 "/etc/dodos/tools/osversion.txt")
 echo "$OS_VERSION"
 log "OS version: $OS_VERSION"
 
@@ -371,47 +368,7 @@ source /etc/bash.bashrc
 rm -rf "$TMP_DIR"
 log "Temporary files cleaned."
 
-# === Create global Desktop Entry ===
-DESKTOP_ENTRY_PATH="/usr/share/applications/1002xTOOLS.desktop"
-if [[ ! -f "$DESKTOP_ENTRY_PATH" ]]; then
-   sudo tee "$DESKTOP_ENTRY_PATH" > /dev/null <<EOF
-[Desktop Entry]
-Name=1002xTOOLS ($VERSION)
-Exec=$SCRIPT_DIR/debui.sh
-Icon=utilities-terminal
-Terminal=true
-Type=Application
-Categories=System;
-EOF
-    sudo chmod +x "$DESKTOP_ENTRY_PATH"
-fi
-
-# === Ensure user Desktop shortcut exists ===
-REALUSER=$(logname 2>/dev/null || echo "$SUDO_USER")
-USER_DESKTOP="$HOME/Desktop"
-[[ -z "$REALUSER" ]] && REALUSER=$(whoami)
-USER_DESKTOP=$(eval echo "~$REALUSER/Desktop")
-mkdir -p "$USER_DESKTOP"
-USER_SHORTCUT="$USER_DESKTOP/1002xTOOLS.desktop"
-
-if [[ ! -f "$USER_SHORTCUT" ]]; then
-    cat <<EOF > "$USER_SHORTCUT"
-[Desktop Entry]
-Name=1002xTOOLS ($VERSION)
-Exec=$SCRIPT_DIR/debui.sh
-Icon=utilities-terminal
-Terminal=true
-Type=Application
-Categories=System;
-EOF
-    chmod +x "$USER_SHORTCUT"
-    chown "$REALUSER":"$REALUSER" "$USER_SHORTCUT"
-fi
-
-
-
-
-whiptail --title "1002xTOOLS Installer" --msgbox "Installtion  completed. If you want uninstall it, just remove /etc/DODOS" 10 50
+whiptail --title "1002xTOOLS Installer" --msgbox "Installtion  completed. If you want uninstall it, just remove /etc/dodos" 10 50
 log "Installation completed."
 
 
